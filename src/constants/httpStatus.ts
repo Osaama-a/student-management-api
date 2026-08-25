92f1f300d4fbbe7f5c6e9fe67cbb2a92c99ff1fc
Oz <usamaahmad0901@gmail.com>
2026-08-25 22:24:59 +0300

    feat: complete database schema and relations

--- src/constants/httpStatus.ts
+++ src/constants/httpStatus.ts
@@ -1,6 +1,7 @@
 export const HTTP_STATUS = {
     OK: 200,
     CREATED: 201,
+    ACCEPTED: 202,
     NO_CONTENT: 204,
     BAD_REQUEST: 400,
     UNAUTHORIZED: 401,
@@ -8,5 +9,10 @@ export const HTTP_STATUS = {
     NOT_FOUND: 404,
     CONFILICT: 409,
     UNPROCESSABLE_ENTITY: 422,
+    TOO_MANY_REQUESTS: 429,
     INTERNAL_SERVER_ERROR: 500,
+    BAD_GETEWAY: 502,
+    SERVICE_UNAVAILABLE: 503,
 } as const; 
+
+export type HttpStatusCode = typeof HTTP_STATUS[keyof typeof HTTP_STATUS];
--- src/db/schema/studentInvoices.ts
+++ src/db/schema/studentInvoices.ts
@@ -1,7 +1,7 @@
 import { pgTable, text, date, timestamp, uuid, unique } from "drizzle-orm/pg-core";
 import { students } from "./students.js";
 import { semesters } from "./semesters.js";
-import  { INVOICE_STATUS, ALL_INVOICE_STATUS} from "../../constants/invoiceStatus.js"
+import  { INVOICE_STATUS, ALL_INVOICE_STATUSES} from "../../constants/invoiceStatus.js"
 
 export const studentInvoices = pgTable("student_invoices", {
     id: uuid("id")
@@ -25,9 +25,9 @@ export const studentInvoices = pgTable("student_invoices", {
     dueDate: date("due_date")
         .notNull(),
 
-    status: text("status", { enum: ALL_INVOICE_STATUS})
+    status: text("status", { enum: ALL_INVOICE_STATUSES})
         .notNull()
-        .default(INVOICE_STATUS.PENDING),
+        .default(INVOICE_STATUS.DRAFT),
 
     createdAt: timestamp("created_at")
         .notNull()
